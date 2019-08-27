90a0a538556dd91c9c48195d29cf0d9ac8b61d46
William White <willcwhite@gmail.com>
2019-08-27 17:29:17 -0400

    table created via sequelize

--- models/Product.js
+++ models/Product.js
@@ -13,4 +13,5 @@ module.exports = function(sequelize, DataTypes){
             type: DataTypes.INTEGER
         }
     })
+    return Product;
 }
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const PORT = 8080;
 
 const app = express();
 
+const db = require("./models");
+
+
+db.sequelize.sync().then(function(){
 app.listen(PORT,function(){
     console.log("Now listening on PORT: "+PORT);
 })
+});
